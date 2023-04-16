7a0afac5ad1b11fb3a846c33a83d5540fd4b2b43
keles-camu <camu.mariangeles@gmail.com>
2023-04-16 12:51:39 +0200

    first commit

---
+++ data-science-tools/R/1_Primer_script.R
@@ -0,0 +1,16 @@
+a <- 5
+b <- 2.0
+c <- '5'
+
+# Comentario de una linea
+
+"
+Comentario de varias lineas
+de
+codigo"
+
+z <- 3
+
+# NOTA: Opcion de comentar y descomentar varias lineas de codigo
+# 1. Selecco?n de las l?neas
+# 2. Sobre las lineas seleccionadas: CNTRL + SHIFT + C
